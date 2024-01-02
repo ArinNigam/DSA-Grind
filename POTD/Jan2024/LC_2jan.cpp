1e2b07659c67002b9d5371ea38c714440d3638c8
Arin Nigam <arin.nigam@gmail.com>
2024-01-02 11:57:33 +0530

    Day 2

--- POTD/Jan2024/LC_2jan.cpp
+++ POTD/Jan2024/LC_2jan.cpp
@@ -1,4 +1,21 @@
-// 11:55 - cutoff
+// Convert an Array Into a 2D Array With Conditions (Medium)
+
+// You are given an integer array nums. You need to create a 2D array from nums satisfying the following conditions:
+
+// The 2D array should contain only the elements of the array nums.
+// Each row in the 2D array contains distinct integers.
+// The number of rows in the 2D array should be minimal.
+// Return the resulting array. If there are multiple answers, return any of them.
+
+// Note that the 2D array can have a different number of elements on each row.
+
+// Input: nums = [1,3,4,1,2,3,1]
+// Output: [[1,3,4,2],[1,3],[1]]
+// Explanation: We can create a 2D array that contains the following rows:
+// - 1,3,4,2
+// - 1,3
+// - 1
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -13,9 +30,6 @@ public:
             mp[nums[i]]++;
             maxi = max(maxi,mp[nums[i]]);
         }
-        // for (auto it:mp){
-        //     cout<<it.first<<" "<<it.second<<endl;
-        // }
         for (int i=0;i<maxi;i++){
             vector<int> temp;
             for (int j=0;j<n;j++){
@@ -23,10 +37,8 @@ public:
                     temp.push_back(nums[j]);
                     mp[nums[j]]--;
                 }
-                // cout<<mp[nums[j]]<<" ";
             }
             ans.push_back(temp);
-            // cout<<endl;
         }
         return ans;
         
