533bea97b81ac8e9ffe87c9b15a98fd5c84ce9a1
Arin Nigam <arin.nigam@gmail.com>
2024-01-06 12:57:25 +0530

    Day 6

--- POTD/Jan2024/LC_6jan.cpp
+++ POTD/Jan2024/LC_6jan.cpp
@@ -1,3 +1,10 @@
+// Maximum Profit in Job Scheduling ( Hard )
+
+// Input: startTime = [1,2,3,3], endTime = [3,4,5,6], profit = [50,10,40,70]
+// Output: 120
+// Explanation: The subset chosen is the first and fourth job. 
+// Time range [1-3]+[3-6] , we get profit of 120 = 50 + 70.
+
 #include<bits/stdc++.h>
 using namespace std;
 
