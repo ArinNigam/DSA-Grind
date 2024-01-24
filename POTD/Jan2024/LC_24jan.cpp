ab1d554c275634483ee5d8253b2c2faf23d1fb98
Arin Nigam <arin.nigam@gmail.com>
2024-01-24 10:36:11 +0530

    Day 24

--- POTD/Jan2024/LC_24jan.cpp
+++ POTD/Jan2024/LC_24jan.cpp
@@ -1,3 +1,15 @@
+// Pseudo-Palindromic Paths in a Binary Tree (Medium)
+// Given a binary tree where node values are digits from 1 to 9. A path in the binary tree is said to be pseudo-palindromic if at least one permutation of the node values in the path is a palindrome.
+// Return the number of pseudo-palindromic paths going from the root node to leaf nodes.
+
+// Input: root = [2,3,1,3,1,null,1]
+// Output: 2 
+// Explanation: The figure above represents the given binary tree. There are three paths going from the root node to leaf nodes: the red path [2,3,3], the green path [2,1,1], and the path [2,3,1]. 
+// Among these paths only red path and green path are pseudo-palindromic paths since the red path [2,3,3] can be rearranged in [3,2,3] (palindrome) and the green path [2,1,1] can be rearranged in [1,2,1] (palindrome).
+
+// HINT: DFS and At most one digit should have an odd frequency.
+// Use bitwise operations to efficiently handle digit occurrences.
+
 #include<bits/stdc++.h>
 using namespace std;
 //------------------------------------------------------------
--- prac.cpp
+++ prac.cpp
@@ -1,113 +1,40 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <utility>
-#include <map>
-#include <set>
-#include <math.h>
-#define ll long long
-#define pb push_back
-#define fr(a, b) for (int i = a; i < b; i++)
-#define rep(i, a, b) for (int i = a; i < b; i++)
-#define mod 1000000007
-#define inf (1LL << 60)
-#define all(x) (x).begin(), (x).end()
-#define prDouble(x) cout << fixed << setprecision(10) << x
-#define triplet pair<ll, pair<ll, ll>>
-#define goog(tno) cout << "Case #" << tno << ": "
-#define fast_io                       \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL)
-#define read(x) \
-    int x;      \
-    cin >> x
+#include<bits/stdc++.h>
 using namespace std;
 
-void init_code()
+int dfs(vector<pair<int,int>>&g, vector<int>& vis, int node, int cost, int target)
 {
-    fast_io;
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
+    if(node==target) return cost;
+    int ans=0;
+    for(auto it: g[node])
+    {
+        int child=it.first;
+        int wt=it.second;
+        if(vis[child])continue;
+        vis[child]=1;
+        ans=max(ans,dfs(g, vis, child, min(cost,wt), target));
+        vis[child]=0;
+    }
+    return min(ans, cost);
 }
 
-int main()
+void solve()
 {
-    init_code();
-
-    int t;
-    cin >> t;
-
-    while (t--)
+    int n,m;
+    cin>>n>>m;
+    int a,b;
+    cin>>a>>b;
+    vector<vector<pair<int,int>>>g(n+1);
+    vector<int>vis(n+1,0);
+    for (int i = 0; i <m; i++)
     {
-        int n;
-        cin >> n;
-
-        vector<int> v;
-        vector<int> p;
-        vector<int> sideLane;
-        vector<int> rem;
-
-        for (int i = 0; i < n; i++)
-        {
-            int x;
-            cin >> x;
-            v.push_back(x);
-        }
-
-        p = v;
-
-        sort(p.begin(), p.end());
-
-        int i = 0;
-        int j = 0;
-        int k = -1;
-
-        while (i < v.size() && j < v.size())
-        {
-            if (v[i] != p[j])
-            {
-                if (k != -1)
-                {
-                    while (sideLane[k] < v[i])
-                    {
-                        rem.push_back(sideLane[k]);
-                        sideLane.pop_back();
-                        k--;
-                    }
-                    sideLane.push_back(v[i]);
-                    i++;
-                    k++;
-                }
-                else
-                {
-                    sideLane.push_back(v[i]);
-                    i++;
-                    k++;
-                }
-            }
-            else
-            {
-                rem.push_back(v[i]);
-                i++;
-                j++;
-            }
-        }
-
-        for (int i = sideLane.size() - 1; i >= 0; i--)
-        {
-            rem.push_back(sideLane[i]);
-        }
-
-        if (rem == p)
-        {
-            cout << "YES" << endl;
-        }
-        else
-        {
-            cout << "NO" << endl;
-        }
+        int x,y,wt;
+        cin>>x>>y>>wt;
+        g[x].push_back({y,wt});
     }
+    cout<<dfs(g, vis, a, INT_MAX, b)<<endl;
+}
+signed main()
+{    
+    solve();
     return 0;
 }
