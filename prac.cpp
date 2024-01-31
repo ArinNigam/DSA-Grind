a230e6d7824eb139a0ca7b9a8ef3b3ec139acc47
Arin Nigam <arin.nigam@gmail.com>
2024-01-31 10:03:24 +0530

    Day31

--- prac.cpp
+++ prac.cpp
@@ -1,52 +1,62 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void merge(vector<int>&nums,int low,int high,int mid){
-    int left = low;
-    int right = mid+1;
-    vector<int>temp;
-    while(left<=mid && right<=high){
-        if (nums[left] > nums[right]){
-            temp.push_back(nums[right]);
-            right++;
+#define ll long long
+
+#define repi(a,b) for (int i=a;i<b;i++)
+#define repj(a,b) for (int j=a;j<b;j++)
+#define repn(a,b) for (int i=a;i>=b;i--)
+
+#define all(v) v.begin(),v.end()
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vl vector<ll>
+#define pii pair<int,int>
+#define vpii vector<pair<int,int>>
+#define mii map<int,int>
+#define ff first
+#define ss second
+
+const int N=1e5+5;
+
+
+void solve()
+{
+    ll a,b,r;
+    cin>>a>>b>>r;
+    if (b>a){
+        swap(a,b);
+    }
+    ll mini = abs(a-b);
+    ll ans = mini;
+    // cout<<(1LL<<63)-1<<" ";
+    ll k =0;
+    repn(63,0){
+        cout<<((b>>i)&1);
+        if (((b>>i)&1) ==0 && ((a>>i)&1)==1){
+            if (k+(1LL<<i)<=r){
+                if (mini>=1LL<<(i+1)){
+                    k+=(1LL<<i);
+                    mini -= (1LL<<(i+1));
+                }
+               
+            }
+         
         }
-        else{
-            temp.push_back(nums[left]);
-            left++;
-        }
     }
-    while(left<=mid){
-        temp.push_back(nums[left]);
-        left++;
-    }
-    while(right<=high){
-        temp.push_back(nums[right]);
-        right++;
-    }
-    for (int i=low;i<=high;i++){
-        nums[i] = temp[i-low];
-    }
-}
-void mergeSort(vector<int>&nums,int low,int high){
-    if (low>=high) return;
-    int mid = (low+high)/2;
-    mergeSort(nums,low,mid);
-    mergeSort(nums,mid+1,high);
-    merge(nums,low,high,mid);
+    cout<<min(mini,ans);
+    
 }
 
 signed main()
 {    
-    int n;
-    cin>>n;
-    vector<int>nums(n);
-    for (int i=0;i<n;i++){
-        cin>>nums[i];
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+        cout<<endl;
     }
-   
-    mergeSort(nums,0,n-1);
-    for (auto it:nums){
-        cout<<it<<" ";
-    }
-    return 0;
+    return 0;    
 }
