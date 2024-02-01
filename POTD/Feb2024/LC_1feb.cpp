0bd8e5d33e4db61a8aac87285c4c0019e8d8f304
Arin Nigam <arin.nigam@gmail.com>
2024-02-01 17:32:19 +0530

    Day32

--- POTD/Feb2024/LC_1feb.cpp
+++ POTD/Feb2024/LC_1feb.cpp
@@ -1,3 +1,11 @@
+// Divide Array Into Arrays With Max Difference (Medium)
+// You are given an integer array nums of size n and a positive integer k.
+
+// Divide the array into one or more arrays of size 3 satisfying the following conditions:
+
+// Each element of nums should be in exactly one array.
+// The difference between any two elements in one array is less than or equal to k.
+
 #include<bits/stdc++.h>
 using namespace std;
 
