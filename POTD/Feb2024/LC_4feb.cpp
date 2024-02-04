015f193f36b4f8e265904e7c87a3441e3f6b6a3b
Arin Nigam <arin.nigam@gmail.com>
2024-02-04 15:18:58 +0530

    Day35

--- POTD/Feb2024/LC_4feb.cpp
+++ POTD/Feb2024/LC_4feb.cpp
@@ -1,3 +1,11 @@
+// Minimum Window Substring (Hard)
+//Given two strings s and t of lengths m and n respectively, return the minimum window substring
+// of s such that every character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
+// Input: s = "ADOBECODEBANC", t = "ABC"
+// Output: "BANC"
+// Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
+
+// HINT : 2 pointer approach use 2 map to track count of characters 
 #include<bits/stdc++.h>
 using namespace std;
 
