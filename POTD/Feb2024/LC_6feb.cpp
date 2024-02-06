9f2e5d72c30efb4c3be27f39ccff67b082e2f53a
Arin Nigam <arin.nigam@gmail.com>
2024-02-06 15:03:02 +0530

    Day37

--- POTD/Feb2024/LC_6feb.cpp
+++ POTD/Feb2024/LC_6feb.cpp
@@ -1,3 +1,13 @@
+// Group Anagrams
+// Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+// Input: strs = ["eat","tea","tan","ate","nat","bat"]
+// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+// TC - O(n*k*logk)
+// k -> str[i].size()
+
 #include<bits/stdc++.h>
 using namespace std;
 
