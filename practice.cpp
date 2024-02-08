4f492308cdd78847e8cdeb3aee4e7f49c797ac6f
Arin Nigam <arin.nigam@gmail.com>
2024-02-08 16:56:28 +0530

    Day39

--- practice.cpp
+++ practice.cpp
@@ -1,37 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Solution {
-public:
-    bool static cmp(pair<int,char>&p1,pair<int,char>&p2){
-        return p1.first > p2.first;
-    }
-    string frequencySort(string s) {
-        int n = s.size();
-        unordered_map<char, int> mp;
-        vector<pair<int,char>>vp;
-        string t;
-        for (int i=0;i<n;i++){
-            mp[s[i]]++;
-        }
-        for (auto it:mp){
-            vp.push_back({it.second,it.first});
-        }
-        sort(vp.begin(),vp.end(),cmp);
-        for (auto it:vp){
-            while(it.first--){
-                t+=it.second;
-            } 
-        }
-        return t;
-    }
-};
+#define ll long long
+
+#define repi(a,b) for (int i=a;i<b;i++)
+#define repj(a,b) for (int j=a;j<b;j++)
+#define repn(a,b) for (int i=a;i>=b;i--)
+
+#define all(v) v.begin(),v.end()
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vl vector<ll>
+#define pii pair<int,int>
+#define vpii vector<pair<int,int>>
+#define mii map<int,int>
+#define ff first
+#define ss second
+
+const int N=1e5+5;
+
+
+void solve()
+{
+    int n,k;
+    cin>>n>>k;
+}
+
 signed main()
 {    
-    string s;
-    cin>>s;
-    Solution ob;
-    auto ans = ob.frequencySort(s);
-    cout<<ans;
-    return 0;
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+        cout<<endl;
+    }
+    return 0;    
 }
