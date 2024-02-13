dd69f4e8cfe08c78cc58794e550674f829e3ade9
Arin Nigam <arin.nigam@gmail.com>
2024-02-13 17:24:37 +0530

    Day44

--- POTD/Feb2024/LC_13feb.cpp
+++ POTD/Feb2024/LC_13feb.cpp
@@ -1,3 +1,11 @@
+// Find First Palindromic String in the Array (Easy)
+// Given an array of strings words, return the first palindromic string in the array. If there is no such string, return an empty string "".
+
+// Input: words = ["abc","car","ada","racecar","cool"]
+// Output: "ada"
+// Explanation: The first string that is palindromic is "ada".
+// Note that "racecar" is also palindromic, but it is not the first.
+
 #include<bits/stdc++.h>
 using namespace std;
 
