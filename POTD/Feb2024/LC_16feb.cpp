c6a6c782088e2cf5c62181f980512957e0a004a5
Arin Nigam <arin.nigam@gmail.com>
2024-02-16 16:00:56 +0530

    Day47

--- POTD/Feb2024/LC_16feb.cpp
+++ POTD/Feb2024/LC_16feb.cpp
@@ -1,3 +1,11 @@
+// Least Number of Unique Integers after K Removals (Medium)
+
+// Given an array of integers arr and an integer k. Find the least number of unique integers after removing exactly k elements.
+
+// Input: arr = [5,5,4], k = 1
+// Output: 1
+// Explanation: Remove the single 4, only 5 is left.
+
 #include<bits/stdc++.h>
 using namespace std;
 
