f7f76e1add21b4333a8ca83bb26ec88ca6495ad5
Arin Nigam <arin.nigam@gmail.com>
2024-02-27 16:06:53 +0530

    Day57

--- POTD/Feb2024/LC_26feb.cpp
+++ POTD/Feb2024/LC_26feb.cpp
@@ -1,3 +1,7 @@
+// Same Tree ( Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+// Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.)
+// Input: p = [1,2,3], q = [1,2,3]
+// Output: true
 #include<bits/stdc++.h>
 using namespace std;
 //------------------------------------------------------------
