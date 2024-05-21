47b7e89f48473b50a6fc4db461cd9e4c4bf6bd3b
Arin Nigam <arin.nigam@gmail.com>
2024-05-21 10:42:42 +0530

    Day 138

---
+++ POTD/May2024/LC_17may.cpp
@@ -0,0 +1,98 @@
+// Delete Leaves With a Given Value (Medium)
+
+// Given a binary tree root and an integer target, delete all the leaf nodes with value target.
+// Note that once you delete a leaf node with value target, if its parent node becomes a leaf node and has the value target, it should also be deleted (you need to continue doing that until you cannot).
+
+// Input: root = [1,2,3,2,null,2,4], target = 2
+// Output: [1,null,3,null,4]
+// Explanation: Leaf nodes in green with value (target = 2) are removed (Picture in left). 
+// After removing, new nodes become leaf nodes with value (target = 2) (Picture in center).
+
+#include<bits/stdc++.h>
+using namespace std;
+//------------------------------------------------------------
+struct TreeNode {
+    int val;
+    struct TreeNode* left;
+    struct TreeNode* right;
+    TreeNode(int x) {
+        val = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void printInorder(TreeNode* node){
+    if(!node) return;
+    printInorder(node->left);
+    cout<<node->val<<" ";
+    printInorder(node->right);
+}
+
+struct TreeNode* buildTree(string str){
+    if (str.length()==0 || str[0]=='N'){
+        return NULL;
+    }
+    vector<string>ip;
+    
+    istringstream iss(str);
+    for (string str;iss>>str;){
+        ip.push_back(str);
+    }
+    TreeNode* root = new TreeNode(stoi(ip[0]));
+    
+    queue<TreeNode*>q;
+    q.push(root);
+    int i = 1;
+    while(!q.empty() && i<ip.size()){
+        TreeNode* currNode = q.front();
+        q.pop();
+        
+        string currVal = ip[i];
+        
+        if (currVal !="N"){
+            currNode->left = new TreeNode(stoi(currVal));
+            
+            q.push(currNode->left);
+        }
+        i++;
+        if (i>=ip.size()){
+            break;
+        }
+        currVal = ip[i];
+        if (currVal !="N"){
+            currNode->right = new TreeNode(stoi(currVal));
+            q.push(currNode->right);
+        }
+        i++;
+    }
+    return root;
+}
+//------------------------------------------------------------
+
+class Solution {
+public:
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        if (root==NULL) return NULL;
+        root->left = removeLeafNodes(root->left,target);
+        root->right = removeLeafNodes(root->right,target);
+        if (root->left == NULL && root->right==NULL && root->val==target){
+            return NULL;
+        }
+        return root;
+    }
+};
+
+signed main()
+{    
+    string s;
+    getline(cin,s,'\n');
+    TreeNode* root = buildTree(s);
+    int target;
+    cin>>target;
+    Solution ob;
+    auto ans = ob.removeLeafNodes(root,target);
+    printInorder(ans);
+    cout<<ans;
+    return 0;
+}
