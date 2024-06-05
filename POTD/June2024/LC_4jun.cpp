e62dceb2365088bb939ac3b6e0bf6e0ace02244a
Arin Nigam <arin.nigam@gmail.com>
2024-06-05 12:12:06 +0530

    Day 156

---
+++ POTD/June2024/LC_4jun.cpp
@@ -0,0 +1,48 @@
+// Longest Palindrome (Easy)
+
+// Given a string s which consists of lowercase or uppercase letters, return the length of the longest 
+// palindrome that can be built with those letters.
+
+// Letters are case sensitive, for example, "Aa" is not considered a palindrome.
+
+// Example 1:
+// Input: s = "abccccdd"
+// Output: 7
+// Explanation: One longest palindrome that can be built is "dccaccd", whose length is 7.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        map<int, int>mp;
+        for (int i=0;i<s.size();i++){
+            mp[s[i]]++;
+        }
+        int cnt = 0;
+        int flag = 0;
+        for (auto it:mp){
+            if (it.second%2==0){
+                 cnt+=it.second;   
+            }
+            else{
+                cnt+=it.second-1;
+                flag = 1;
+            }
+        }
+        return cnt+flag;
+    }
+};
+
+
+signed main()
+{    
+    string s;
+    cin>>s;
+    Solution ob;
+    auto ans = ob.longestPalindrome(s);
+    cout<<ans;
+    return 0;
+}3
