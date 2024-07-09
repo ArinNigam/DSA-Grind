4bdb2b62023c1b31552a589dee446073f7f31c08
Arin Nigam <arin.nigam@gmail.com>
2024-07-09 11:30:54 +0530

    Day 191

--- Graphs and Trees/topoSort.cpp
+++ Graphs and Trees/topoSort.cpp
@@ -1,43 +1,48 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define endl '\n'
-#define ll long long
-const int N=1e5+5;
-vector<int>nums[N];
-
-void dfs(int node,stack<int>&st, vector<vector<int>>&g, vector<int>&vis){
-  vis[node]=1;
-  for (auto it : g[node]){
-    if(!vis[it]) dfs(it,st,g,vis);
+class Solution {
+public:
+  void dfs(int node,stack<int>&st,vector<int>&vis,vector<vector<int>>&adj){
+    vis[node] = 1;
+    for(auto it:adj[node]){
+      if (!vis[it]){
+        dfs(it,st,vis,adj);
+      }
+    }
+    st.push(node);
   }
-  st.push(node);
-}  
+  int topoSort(vector<vector<int>>&nums){
+    int n = nums.size();
+    vector<vector<int>>adj(n);
+    vector<int>vis(n,0);
+    stack<int>st;
+    for (int i=0;i<n;i++){
+      if (!vis[i]){
+        dfs(i,st,vis,adj);
+      }
+    }
+    vector<int>ans;
+    while(!st.empty()){
+      ans.push_back(st.top());
+      st.pop();
+    }
+    for (auto i: ans){
+      cout<<i<<' ';
+    }
+  }
+};
+
 signed main()
 {  
   int n;
   cin>>n;
-  vector<vector<int>>g(n);
+  vector<vector<int>>nums(n,vector<int>(2));
   for (int i=0;i<n;i++){
-    int u,v;
-    cin>>u>>v;
-    g[v].push_back(u);
+    cin>>nums[i][0]>>nums[i][1];
   }
-  vector<int>vis(n,0);
-  stack<int>st;
-  for (int i=0;i<n;i++){
-    if (!vis[i]){
-      dfs(i,st,g,vis);
-    }
-  }
-  vector<int>ans;
-  while(!st.empty()){
-    ans.push_back(st.top());
-    st.pop();
-  }
-  for (auto i: ans){
-    cout<<i<<' ';
-  }
-  
+  Solution ob;
+  auto ans = ob.topoSort(nums);
+  cout<<ans;
   return 0;
 }
