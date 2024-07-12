3de2b71131cd59b4488e0874acd32939897c5b4d
Arin Nigam <arin.nigam@gmail.com>
2024-07-12 18:25:32 +0530

    Day 194

---
+++ POTD/July2024/LC_12jul.cpp
@@ -0,0 +1,104 @@
+// Maximum Score From Removing Substrings (Medium)
+
+// You are given a string s and two integers x and y. You can perform two types of operations any number of times.
+
+// Remove substring "ab" and gain x points.
+// For example, when removing "ab" from "cabxbae" it becomes "cxbae".
+// Remove substring "ba" and gain y points.
+// For example, when removing "ba" from "cabxbae" it becomes "cabxe".
+// Return the maximum points you can gain after applying the above operations on s.
+
+// Input: s = "cdbcbbaaabab", x = 4, y = 5
+// Output: 19
+// Explanation:
+// - Remove the "ba" underlined in "cdbcbbaaabab". Now, s = "cdbcbbaaab" and 5 points are added to the score.
+// - Remove the "ab" underlined in "cdbcbbaaab". Now, s = "cdbcbbaa" and 4 points are added to the score.
+// - Remove the "ba" underlined in "cdbcbbaa". Now, s = "cdbcba" and 5 points are added to the score.
+// - Remove the "ba" underlined in "cdbcba". Now, s = "cdbc" and 5 points are added to the score.
+// Total score = 5 + 4 + 5 + 5 = 19.
+
+class Solution {
+public:
+    int maximumGain(string s, int x, int y) {
+        stack<char>st;
+        int n = s.length();
+        long long sum = 0;
+        for(int i=0;i<n;i++){
+            if (x>y){
+                if(s[i]=='b'){
+                    if(!st.empty() && st.top() == 'a'){
+                        sum+=x;
+                        st.pop();
+                    }
+                    else{
+                        st.push(s[i]);
+                    }
+                }
+                else{
+                    st.push(s[i]);
+                }
+            }
+            else{
+                if(s[i]=='a'){
+                    if(!st.empty() && st.top() == 'b'){
+                        sum+=y;
+                        st.pop();
+                    }
+                    else{
+                        st.push(s[i]);
+                    }
+                }
+                else{
+                    st.push(s[i]);
+                }
+                
+            }
+        }
+        string t = "";
+        while (!st.empty()) {
+            t+= st.top();
+            st.pop();
+        }
+        reverse(t.begin(), t.end());
+
+        for (int i=0; i<t.size(); i++) {
+            if (x > y) {
+                if (t[i] == 'a') {
+                    if (!st.empty() && st.top() == 'b') {
+                        sum += y;
+                        st.pop();
+                    } else {
+                        st.push(t[i]);
+                    }
+                } else {
+                    st.push(t[i]);
+                }
+            } else {
+                if (t[i] == 'b') {
+                    if (!st.empty() && st.top() == 'a') {
+                        sum += x;
+                        st.pop();
+                    } else {
+                        st.push(t[i]);
+                    }
+                } else {
+                    st.push(t[i]);
+                }
+            }
+        }
+        return sum;
+
+    }
+};
+
+signed main()
+{    
+    string s;
+    cin>>s;
+    int x,y;
+    cin>>x>>y;
+    Solution ob;
+    auto ans = ob.maximumGain(s,x,y);
+    cout<<ans;
+    return 0;
+}
