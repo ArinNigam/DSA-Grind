714b4dc416b11fa66e922bf1b900ea03e8505e05
Arin Nigam <arin.nigam@gmail.com>
2024-09-01 22:33:05 +0530

    Day 245

---
+++ POTD/Sept2024/LC_1sept.cpp
@@ -0,0 +1,40 @@
+// Convert 1D Array Into 2D Array (Easy)
+
+// You are given a 0-indexed 1-dimensional (1D) integer array original, and two integers, m and n. You are tasked with creating a 2-dimensional (2D) array with  m rows and n columns using all the elements from original.
+// The elements from indices 0 to n - 1 (inclusive) of original should form the first row of the constructed 2D array, the elements from indices n to 2 * n - 1 (inclusive) should form the second row of the constructed 2D array, and so on.
+// Return an m x n 2D array constructed according to the above procedure, or an empty 2D array if it is impossible.
+
+// Input: original = [1,2,3,4], m = 2, n = 2
+// Output: [[1,2],[3,4]]
+// Explanation: The constructed 2D array should contain 2 rows and 2 columns.
+// The first group of n=2 elements in original, [1,2], becomes the first row in the constructed 2D array.
+// The second group of n=2 elements in original, [3,4], becomes the second row in the constructed 2D array.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        int l = original.size();
+        vector<vector<int>>ans(m,vector<int>(n));
+        for(int i=0;i<l;i++){
+            ans[i/n][i%n] = original[i];
+        }
+        return ans;
+    }
+};
+
+signed main()
+{    
+    int m,n;
+    cin>>m>>n;
+    vector<int> original(m*n);
+    for(int i=0;i<m*n;i++){
+        cin>>original[i];
+    }
+    Solution ob;
+    auto ans = ob.construct2DArray(original,m,n);
+    print(ans);
+    return 0;
+}
